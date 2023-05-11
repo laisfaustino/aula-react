d5a6a98033fb8e63b8c55f21b78540a90275715f
laisfaustino <laisfaustino@hotmail.com>
2023-05-11 17:24:47 -0300

    Primeira aula React

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home(){
+    return(
+   <>
+        <h1 className='titulo'>Hello world React</h1>
+        <img className='img' src="https://media.tenor.com/eQ0IJ1YaBN4AAAAd/aespa-gg.gif" alt="" />
+   </>
+   )
+}
+
+export default Home;
