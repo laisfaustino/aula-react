88267a7cf3f7dee567e39ff6896ecb906ea142b3
laisfaustino <laisfaustino@hotmail.com>
2023-05-12 11:42:59 -0300

    Grid/FlexBox

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './Home.css'
+import { Grid , Paper ,Box, Button } from '@mui/material';
 
 function Home(){
     return(
    <>
-        <h1 className='titulo'>Hello world React</h1>
-        <img className='img' src="https://media.tenor.com/eQ0IJ1YaBN4AAAAd/aespa-gg.gif" alt="" />
+        <Paper>
+          <Box p={2}>
+               <Box display="flex" justifyContent="center">
+                    <h1>Titulo</h1>
+               </Box>
+               <img src="https://i.pinimg.com/originals/da/81/0d/da810d2a31e40a5747cbe51ded08330f.gif" alt="Cactus" style={{width: "100%" ,height: "100%"}} />
+               <Box display="flex" justifyContent="center" p={2}>
+                    <Button variant='contained' color="primary">Aceitar</Button>
+                    <Button variant='contained' color="secondary">Recusar</Button></Box>
+          </Box>
+        </Paper>
    </>
    )
 }
